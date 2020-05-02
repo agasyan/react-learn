f3f3f63730cac34af3f5f3cbf6f2bd72c79e4f33
Agas Yanpratama <agas.yanpratama@gmail.com>
2020-05-02 12:41:10 +0700

    fix cannot use es 6

--- deploy/server.js
+++ deploy/server.js
@@ -1,13 +1,14 @@
-import { join } from 'path';
-import express, { static } from 'express';
+const path = require('path');
+const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, '..', 'build');
 
-app.use(static(publicPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(join(publicPath, 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
