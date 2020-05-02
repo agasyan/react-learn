270cdad6e8d526e99e218c2a7659b75bedfbab0f
Agas Yanpratama <agas.yanpratama@gmail.com>
2020-05-02 12:29:27 +0700

    Import express and path

--- deploy/server.js
+++ deploy/server.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const express = require('express');
+import { join } from 'path';
+import express, { static } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = join(__dirname, '..', 'build');
 
-app.use(express.static(publicPath));
+app.use(static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo3.svg';
 import './App.css';
 
 function App() {
